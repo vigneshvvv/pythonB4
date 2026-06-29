b6db8d677ce8d3bcec3cb2e245f35f9ccf5cbd56
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-06-29 18:24:05 +0530

    Add comments class and example instances
    Define a comments class and create two comment instances with properties.

---
+++ commentsObject.py
@@ -0,0 +1,20 @@
+class comments:
+    id = 0
+    body = ""
+    postId = 0
+    likes = 0
+
+
+comment1 = comments()
+comment1.id = 1
+comment1.body = "sfdfsfsdfsfsdf"
+comment1.postId = 1
+comment1.likes = 100
+
+print(comment1.likes)
+
+comment2 = comments()
+comment2.id = 2
+comment2.body = "afsdsadsasffdbvvxvc"
+comment2.postId = 2
+comment2.likes = 50
